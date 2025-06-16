4f5bb07536d91a12c7fe5f7aa453c6c6efe60a92
Felipe Lalanne <felipe@balena.io>
2025-06-16 15:33:00 -0400

    Refactor code and rename repository
    Change-type: patch

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,30 @@
-use std::net::SocketAddr;
+use anyhow::{Context, Result};
+use hyper::Uri;
+use std::env;
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 pub struct Config {
-    pub listen_addr: SocketAddr,
-    pub balena_api_endpoint: String,
-    pub legacy_supervisor_url: String,
+    pub local_api_port: u16,
+    pub balena_api_uri: Uri,
+    pub legacy_supervisor_uri: Uri,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self> {
+        let local_api_port = env::var("BALENA_SUPERVISOR_PORT")
+            .unwrap_or_else(|_| "48484".to_string())
+            .parse::<u16>()?;
+        let balena_api_uri = env::var("BALENA_API_ENDPOINT")
+            .unwrap_or_else(|_| "https://api.balena-cloud.com".to_string())
+            .parse()?;
+        let legacy_supervisor_uri = env::var("LEGACY_SUPERVISOR_ADDRESS")
+            .with_context(|| "LEGACY_SUPERVISOR_ADDRESS undefined")?
+            .parse()?;
+
+        Ok(Self {
+            local_api_port,
+            balena_api_uri,
+            legacy_supervisor_uri,
+        })
+    }
 }
