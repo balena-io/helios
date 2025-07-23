736a2a4be1eac1d0e8872d2c35aa43be76f9e4cc
Akis Kesoglou <akiskesoglou@gmail.com>
2025-07-23 15:06:14 +0300

    Flesh out the CLI providing defaults and encoding dependencies between arguments
    Change-type: patch

--- src/cli.rs
+++ src/cli.rs
@@ -1,11 +1,23 @@
 use axum::http::Uri;
 use clap::{Parser, Subcommand};
+use std::convert::Infallible;
+use std::num::ParseIntError;
 use std::path::PathBuf;
 use std::time::Duration;
 use std::{fs, io};
 use tracing::debug;
 
 use crate::config::{Config, LocalAddress};
+use crate::state::models::Uuid;
+
+fn parse_uuid(s: &str) -> Result<Uuid, Infallible> {
+    Ok(s.to_string().into())
+}
+
+fn parse_duration(s: &str) -> Result<Duration, ParseIntError> {
+    let millis: u64 = s.parse()?;
+    Ok(Duration::from_millis(millis))
+}
 
 #[derive(Clone, Debug, Parser)]
 #[command(version, about, long_about = None)] // read from Cargo.toml
@@ -13,82 +25,98 @@ struct Cli {
     #[command(subcommand)]
     command: Option<Command>,
 
+    /// Device UUID
     #[arg(
         long = "uuid",
         value_name = "uuid",
-        help = "Device UUID",
+        value_parser = parse_uuid,
         env = "HELIOS_UUID"
     )]
-    uuid: Option<String>,
+    uuid: Option<Uuid>,
 
+    /// Local API listen address
     #[arg(
         long = "local-address",
         value_name = "address",
-        help = "Local API listen address",
-        env = "HELIOS_LOCAL_ADDRESS"
+        env = "HELIOS_LOCAL_ADDRESS",
+        default_value_t
     )]
-    local_address: Option<LocalAddress>,
+    local_address: LocalAddress,
 
+    /// Remote API endpoint
     #[arg(
         long = "remote-api-endpoint",
         value_name = "uri",
-        help = "Remote API endpoint",
         env = "HELIOS_REMOTE_API_ENDPOINT"
     )]
     remote_api_endpoint: Option<Uri>,
 
+    /// API key to use for authentication with remote
     #[arg(
         long = "remote-api-key",
         value_name = "key",
-        help = "Remote API key",
-        env = "HELIOS_REMOTE_API_KEY"
+        env = "HELIOS_REMOTE_API_KEY",
+        requires = "remote_api_endpoint"
     )]
     remote_api_key: Option<String>,
 
+    /// Remote request timeout in milliseconds
     #[arg(
         long = "remote-request-timeout-ms",
         value_name = "request_timeout_ms",
-        help = "Remote API request timeout in milliseconds",
-        env = "HELIOS_REMOTE_REQUEST_TIMEOUT_MS"
+        value_parser = parse_duration,
+        env = "HELIOS_REMOTE_REQUEST_TIMEOUT_MS",
+        requires = "remote_api_endpoint",
+        default_value = "59000"
     )]
-    remote_request_timeout_ms: Option<u64>,
+    remote_request_timeout_ms: Duration,
 
+    /// Remote poll interval in milliseconds
     #[arg(
         long = "remote-poll-interval-ms",
         value_name = "poll_interval_ms",
-        help = "Remote API poll interval in milliseconds",
-        env = "HELIOS_REMOTE_POLL_INTERVAL_MS"
+        value_parser = parse_duration,
+        env = "HELIOS_REMOTE_POLL_INTERVAL_MS",
+        requires = "remote_api_endpoint",
+        default_value = "900000"
     )]
-    remote_poll_interval_ms: Option<u64>,
+    remote_poll_interval_ms: Duration,
 
+    /// Remote rate limiting interval in milliseconds
     #[arg(
         long = "remote-min-interval-ms",
         value_name = "min_interval_ms",
-        help = "API rate limiting interval in milliseconds",
-        env = "HELIOS_REMOTE_MIN_INTERVAL_MS"
+        value_parser = parse_duration,
+        env = "HELIOS_REMOTE_MIN_INTERVAL_MS",
+        requires = "remote_api_endpoint",
+        default_value = "10000"
     )]
-    remote_min_interval_ms: Option<u64>,
+    remote_min_interval_ms: Duration,
 
+    /// Remote target state poll max jitter in milliseconds
     #[arg(
         long = "remote-max-poll-jitter-ms",
         value_name = "max_poll_jitter_ms",
-        help = "API target state poll max jitter in milliseconds",
-        env = "HELIOS_REMOTE_MAX_POLL_JITTER_MS"
+        value_parser = parse_duration,
+        env = "HELIOS_REMOTE_MAX_POLL_JITTER_MS",
+        requires = "remote_api_endpoint",
+        default_value = "60000"
     )]
-    remote_max_poll_jitter_ms: Option<u64>,
+    remote_max_poll_jitter_ms: Duration,
 
+    /// Fallback URI to redirect unsupported API requests
     #[arg(
         long = "fallback-address",
         value_name = "uri",
-        help = "Fallback URI to redirect unsupported API requests",
         env = "HELIOS_FALLBACK_ADDRESS"
     )]
     fallback_address: Option<Uri>,
 
+    /// API key to use for authentication with fallback
     #[arg(
         long = "fallback-api-key",
         value_name = "key",
-        help = "API key to perform requests to fallback URI",
+        requires = "fallback_address",
         env = "HELIOS_FALLBACK_API_KEY"
     )]
     fallback_api_key: Option<String>,
@@ -98,11 +126,8 @@ struct Cli {
 pub enum Command {
     /// Provision device to remote endpoint (TODO!)
     Register {
-        #[arg(
-            long = "provisioning-key",
-            value_name = "key",
-            help = "Provisioning key"
-        )]
+        /// Provisioning key
+        #[arg(long = "provisioning-key", value_name = "key")]
         provisioning_key: String,
     },
 }
@@ -125,29 +150,21 @@ pub fn load() -> io::Result<(Option<Command>, Config)> {
 
     // Apply CLI overrides in order of precedence
     if let Some(uuid) = cli.uuid {
-        config.uuid = uuid.into();
+        config.uuid = uuid;
     }
-    if let Some(address) = cli.local_address {
-        config.local_address = address;
-    }
+    config.local_address = cli.local_address;
+
     if let Some(endpoint) = &cli.remote_api_endpoint {
         config.remote.api_endpoint = Some(endpoint.clone());
     }
     if let Some(key) = &cli.remote_api_key {
         config.remote.api_key = Some(key.clone());
     }
-    if let Some(interval) = cli.remote_poll_interval_ms {
-        config.remote.poll_interval = Duration::from_millis(interval);
-    }
-    if let Some(timeout) = cli.remote_request_timeout_ms {
-        config.remote.request_timeout = Duration::from_millis(timeout);
-    }
-    if let Some(min_interval) = cli.remote_min_interval_ms {
-        config.remote.min_interval = Duration::from_millis(min_interval);
-    }
-    if let Some(jitter) = cli.remote_max_poll_jitter_ms {
-        config.remote.max_poll_jitter = Duration::from_millis(jitter);
-    }
+    config.remote.request_timeout = cli.remote_request_timeout_ms;
+    config.remote.poll_interval = cli.remote_poll_interval_ms;
+    config.remote.min_interval = cli.remote_min_interval_ms;
+    config.remote.max_poll_jitter = cli.remote_max_poll_jitter_ms;
+
     if let Some(address) = &cli.fallback_address {
         config.fallback.address = Some(address.clone());
     }
