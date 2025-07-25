d48bbd6979f13da4e535111fecff89e55008d3e2
Akis Kesoglou <akiskesoglou@gmail.com>
2025-07-25 11:55:44 +0300

    Make utility digest function
    Change-type: patch

---
+++ src/util/crypto.rs
@@ -0,0 +1,8 @@
+use sha2::{Digest as _, Sha256};
+
+pub fn sha256_hex_digest<D: AsRef<[u8]>>(data: D) -> String {
+    let mut hasher = Sha256::default();
+    hasher.update(data.as_ref());
+    let digest = hasher.finalize();
+    format!("{:x}", digest)
+}
