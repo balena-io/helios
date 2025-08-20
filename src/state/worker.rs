4bdb476b96d7aea1f72d8ee69d82db5cf266a2e5
felipe <felipe@balena.io>
2025-08-20 17:05:02 -0400

    Add image pull/remove tasks to worker
    Change-type: minor

--- src/state/worker.rs
+++ src/state/worker.rs
@@ -1,6 +1,10 @@
-use bollard::Docker;
+use bollard::query_parameters::RemoveImageOptions;
+use bollard::{query_parameters::CreateImageOptions, Docker};
+use futures_lite::StreamExt;
+use thiserror::Error;
 
-use mahler::extract::{Pointer, Target, View};
+use mahler::extract::{Pointer, Res, System, Target, View};
+use mahler::task::{with_io, Create, Delete};
 use mahler::worker::Uninitialized;
 use mahler::{
     extract::Args,
@@ -9,8 +13,16 @@ use mahler::{
 };
 
 use crate::types::Uuid;
+use crate::util::docker::ImageUri;
 
-use super::models::{App, Device, DeviceConfig, Release, TargetApp, TargetDevice};
+use super::models::{App, Device, DeviceConfig, Image, Release, TargetApp, TargetDevice};
+
+#[derive(Error, Debug)]
+#[error("{context}: {source}")]
+pub struct DockerError {
+    context: String,
+    source: bollard::errors::Error,
+}
 
 /// Update the in-memory device name
 fn set_device_name(
@@ -55,6 +67,132 @@ fn prepare_release(mut release: Pointer<Release>) -> Pointer<Release> {
     release
 }
 
+/// Pull an image from the registry, this task is applicable to
+/// the creation of a new image
+///
+/// Condition: the image is not already present in the device
+/// Effect: add the image to the list of images
+/// Action: pull the image from the registry and add it to the images local registry
+fn pull_image(
+    mut image: Pointer<Image>,
+    Args(image_name): Args<ImageUri>,
+    docker: Res<Docker>,
+) -> Create<Image, DockerError> {
+    // Initialize the image if it doesn't exist
+    if image.is_none() {
+        image.get_or_insert_default();
+    }
+
+    with_io(image, |mut image| async move {
+        let docker = docker
+            .as_ref()
+            .expect("docker resource should be available");
+        // Check if the image exists first, we do this because
+        // we don't know if the initial state is correct
+        match docker.inspect_image(image_name.as_str()).await {
+            Ok(img_info) => {
+                if img_info.id.is_some() {
+                    // If the image exists and has an id, skip
+                    // download
+                    image.replace(img_info.into());
+                    return Ok(image);
+                }
+            }
+            Err(e) => {
+                if let bollard::errors::Error::DockerResponseServerError { status_code, .. } = e {
+                    if status_code != 404 {
+                        return Err(DockerError {
+                            context: format!("failed to read information for image {image_name}"),
+                            source: e,
+                        });
+                    }
+                } else {
+                    return Err(DockerError {
+                        context: format!("failed to read information for image {image_name}"),
+                        source: e,
+                    });
+                }
+            }
+        }
+
+        // Otherwise try to download the image
+        let options = Some(CreateImageOptions {
+            from_image: Some(image_name.clone().into()),
+            ..Default::default()
+        });
+
+        // Try to create the image
+        let mut stream = docker.create_image(options, None, None);
+        while let Some(progress) = stream.next().await {
+            // TODO: report progress. This requires https://github.com/balena-io-modules/mahler-rs/issues/43
+            let _ = progress.map_err(|e| DockerError {
+                context: format!("failed to download image {image_name}"),
+                source: e,
+            })?;
+        }
+
+        // Check that the image
+        let img_info = docker
+            .inspect_image(image_name.as_str())
+            .await
+            .map_err(|e| DockerError {
+                context: format!("failed to read information for image {image_name}"),
+                source: e,
+            })?;
+
+        image.replace(img_info.into());
+
+        Ok(image)
+    })
+}
+
+/// Remove an image
+///
+/// Condition: the image exists (and there are no services referencing it?)
+/// Effect: remove the image from the state
+/// Action: remove the image from the engine
+fn remove_image(
+    img_ptr: Pointer<Image>,
+    Args(image_name): Args<ImageUri>,
+    System(device): System<Device>,
+    docker: Res<Docker>,
+) -> Delete<Image, DockerError> {
+    // only remove the image if it not being used by any service
+    if device.apps.values().any(|app| {
+        app.releases.values().any(|release| {
+            release
+                .services
+                .values()
+                .any(|s| s.image == image_name.clone())
+        })
+    }) {
+        return img_ptr.into();
+    }
+
+    with_io(img_ptr, |img_ptr| async move {
+        docker
+            .as_ref()
+            .expect("docker resource should be available")
+            .remove_image(
+                image_name.as_str(),
+                Option::<RemoveImageOptions>::None,
+                None,
+            )
+            .await
+            .map_err(|source| DockerError {
+                context: format!("failed to remove image {image_name}"),
+                source,
+            })?;
+
+        Ok(img_ptr)
+    })
+    .map(|mut img| {
+        // delete the image pointer
+        img.take();
+        img
+    })
+}
+
 #[derive(Debug, thiserror::Error)]
 pub enum CreateError {
     #[error("Failed to connect to Docker daemon: {0}")]
@@ -72,6 +210,17 @@ fn worker() -> Worker<Device, Uninitialized, TargetDevice> {
         .job(
             "/name",
             task::any(set_device_name).with_description(|| "update device name"),
+        )
+        .jobs(
+            "/images/{image_name}",
+            [
+                task::create(pull_image).with_description(|Args(image_name): Args<String>| {
+                    format!("pull image '{image_name}'")
+                }),
+                task::delete(remove_image).with_description(|Args(image_name): Args<String>| {
+                    format!("delete image '{image_name}'")
+                }),
+            ],
         )
         .job(
             "/apps/{app_uuid}",
