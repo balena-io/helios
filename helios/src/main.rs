9a2ee2d50cd4dd65c99537e2a9b550b8b957ac38
Felipe Lalanne <felipe@balena.io>
2026-06-09 13:00:33 -0400

    Configure logs output by level
    Logs with level warn and above go to stderr and the rest go to stdout,
    this will ensure they get the right priority on the journal and will
    help with future filtering.
    
    Change-type: patch

--- helios/src/main.rs
+++ helios/src/main.rs
@@ -4,10 +4,10 @@ use std::time::Duration;
 
 use tokio::net::{TcpListener, UnixListener};
 use tokio::sync::watch::{self};
-use tracing::{debug, info, instrument, trace, warn};
+use tracing::{Level, debug, error, info, trace, warn};
 use tracing_subscriber::{
     EnvFilter,
-    fmt::{self, format::FmtSpan},
+    fmt::{format::FmtSpan, layer as fmt_layer, writer::MakeWriterExt},
     layer::SubscriberExt,
     util::SubscriberInitExt,
 };
@@ -48,8 +48,13 @@ fn initialize_tracing() {
             ),
         )
         .with(
-            fmt::layer()
-                .with_writer(std::io::stderr)
+            fmt_layer()
+                // INFO and below go to stdout; WARN and ERROR go to stderr.
+                .with_writer(
+                    std::io::stderr
+                        .with_max_level(Level::WARN)
+                        .or_else(std::io::stdout),
+                )
                 .with_span_events(FmtSpan::CLOSE)
                 .event_format(fmt::format().compact().with_target(false).without_time()),
         )
