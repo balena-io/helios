84d3ed479e4eeb9d303a1d68873633d3ae68f676
Felipe Lalanne <felipe@balena.io>
2026-07-10 11:05:37 -0400

    Include release uuid in rejection warning
    This should allow us to identify if helios is processing a stale target
    state.
    
    Change-type: patch

--- helios-remote/src/poll.rs
+++ helios-remote/src/poll.rs
@@ -250,7 +250,8 @@ async fn start_poll_with_reemit(
                                 for (uuid, app) in remote_target.apps.iter() {
                                     if let RemoteApp::Rejected(rej) = app {
                                         warn!(
-                                            "invalid target release for app {uuid}: {}",
+                                            "invalid target release ({}) for app {uuid}: {}",
+                                            rej.release.chars().take(7).collect::<String>(),
                                             rej.reason,
                                         );
                                     }
